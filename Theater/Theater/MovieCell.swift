467a0a3d80899a2c9aa44170d4336b9c661e1608
Tarang Khanna <khanna17@purdue.edu>
2016-01-13 07:46:32 -0500

    initial setup done
    QuickStart Videos done

---
+++ Theater/Theater/MovieCell.swift
@@ -0,0 +1,30 @@
+//
+//  MovieCell.swift
+//  Theater
+//
+//  Created by Tarang khanna on 1/13/16.
+//  Copyright © 2016 Tarang khanna. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var overviewLabel: UILabel!
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
