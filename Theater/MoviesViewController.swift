e095f20480a2d3d91280a3e46342bd021c98f856
Tarang Khanna <khanna17@purdue.edu>
2016-01-28 13:33:38 -0500

    tab bar controller
    now playing and top rated

--- Theater/MoviesViewController.swift
+++ Theater/MoviesViewController.swift
@@ -27,6 +27,7 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
     
     @IBOutlet weak var loadedOutlet: UIBarButtonItem!
     
+    var endpoint: String = ""
     var movies: [NSDictionary]?
     var searchActive : Bool = false
     var allLoaded : Bool = false
@@ -139,7 +140,7 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
     
     func retrieve() {
         let apiKey = "a07e22bc18f5cb106bfe4cc1f83ad8ed"
-        let url = NSURL(string:"https://api.themoviedb.org/3/movie/now_playing?api_key=\(apiKey)")
+        let url = NSURL(string:"https://api.themoviedb.org/3/movie/\(endpoint)?api_key=\(apiKey)")
         let request = NSURLRequest(URL: url!)
         let session = NSURLSession(
             configuration: NSURLSessionConfiguration.defaultSessionConfiguration(),
